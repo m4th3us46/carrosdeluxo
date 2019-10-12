bc2e01164a303d58f846171afde8082e6ff987e4
m4th3us46 <theus.cruzf@gmail.com>
2019-10-12 16:24:36 -0300

    Trabalho de escola

---
+++ script/js.js
@@ -0,0 +1,26 @@
+function navegacao(y){
+    const carro1 = document.getElementById("pA");
+    const carro2 = document.getElementById("pB");
+    const carro3 = document.getElementById("pC");
+
+    if (y === 1){
+
+        carro1.style.display="block";
+        carro2.style.display="none";
+        carro3.style.display="none";    
+    
+    }else if(y === 2){
+
+        carro1.style.display="none";
+        carro2.style.display="block";
+        carro3.style.display="none";    
+    
+    }else {
+
+        carro1.style.display="none";
+        carro2.style.display="none";
+        carro3.style.display="block";    
+
+    }
+
+}
