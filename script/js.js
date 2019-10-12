229ab87bdfc3aa2e1c2b8338f3d4a95368cc9450
m4th3us46 <theus.cruzf@gmail.com>
2019-10-12 18:03:33 -0300

    Ajustes

--- script/js.js
+++ script/js.js
@@ -5,21 +5,21 @@ function navegacao(y){
 
     if (y === 1){
 
-        carro1.style.display="block";
+        carro1.style.display="flex";
         carro2.style.display="none";
         carro3.style.display="none";    
     
     }else if(y === 2){
 
         carro1.style.display="none";
-        carro2.style.display="block";
+        carro2.style.display="flex";
         carro3.style.display="none";    
     
     }else {
 
         carro1.style.display="none";
         carro2.style.display="none";
-        carro3.style.display="block";    
+        carro3.style.display="flex";    
 
     }
 
