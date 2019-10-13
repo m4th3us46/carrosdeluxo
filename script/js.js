f75d4ea8e021d379fc5c5acb46831f3df92327e9
m4th3us46 <theus.cruzf@gmail.com>
2019-10-13 17:50:05 -0300

    Ajustes

--- script/js.js
+++ script/js.js
@@ -1,25 +1,25 @@
 function navegacao(y){
-    const carro1 = document.getElementById("pA");
-    const carro2 = document.getElementById("pB");
-    const carro3 = document.getElementById("pC");
 
     if (y === 1){
 
-        carro1.style.display="flex";
-        carro2.style.display="none";
-        carro3.style.display="none";    
+            
+    document.getElementById("pA").style.display="flex";
+    document.getElementById("pB").style.display="none";
+    document.getElementById("pC").style.display="none";    
     
     }else if(y === 2){
 
-        carro1.style.display="none";
-        carro2.style.display="flex";
-        carro3.style.display="none";    
+            
+    document.getElementById("pA").style.display="none";
+    document.getElementById("pB").style.display="flex";
+    document.getElementById("pC").style.display="none";    
     
     }else {
 
-        carro1.style.display="none";
-        carro2.style.display="none";
-        carro3.style.display="flex";    
+           
+    document.getElementById("pA").style.display="none";
+    document.getElementById("pB").style.display="none"
+    document.getElementById("pC").style.display="flex"; 
 
     }
 
